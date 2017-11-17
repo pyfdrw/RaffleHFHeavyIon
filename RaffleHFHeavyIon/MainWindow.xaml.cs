87fb8aec1c5a529c24773c47243cd492942df6d6
tobepyf <tobepyf@hotmail.com>
2017-11-17 22:42:50 +0800

    初始抽奖代码

---
+++ RaffleHFHeavyIon/MainWindow.xaml.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Uri = System.Uri;
+
+namespace RaffleHFHeavyIon
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        // 抽奖使用的类
+        private RaffleHelper raffle;
+
+        private BackgroundWorker backgroundWorker;
+
+        // 抽奖的背景音乐
+        private MediaPlayer progressPlayer = new MediaPlayer();
+        // 抽奖结束的背景音乐
+        private MediaPlayer finalPlayer = new MediaPlayer();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            backgroundWorker = (BackgroundWorker) this.FindResource("BackgroundWorker");
+
+            raffle = new RaffleHelper(10, 14);
+            this.DataContext = raffle;
+
+            progressPlayer.Open(new Uri("欢乐抽奖.mp3", UriKind.Relative));
+            finalPlayer.Open(new Uri("guanzhonghuhan.mp3", UriKind.Relative));
+        }
+
+        private void BackgroundWorker_OnDoWork(object sender, DoWorkEventArgs e)
+        {
+            while (true)
+            {
+                raffle.GenerateRandomSequentially();
+                if (backgroundWorker.CancellationPending)
+                {
+                    e.Cancel = true;
+                    return;
+                }
+            }
+            
+        }
+
+        private void BackgroundWorker_OnRunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Cancelled)
+            {
+                raffle.AddHistory();
+            }
+        }
+
+        // 为false，说明是开始
+        // 为true，说明是暂停
+        private bool workStatus = false;
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // 点击的是开始
+            if (!workStatus)
+            {
+                Button.Content = "暂停";
+
+                progressPlayer.MediaEnded -= new EventHandler(Media_Ended);
+                progressPlayer.MediaEnded += new EventHandler(Media_Ended);
+                progressPlayer.Play();
+                finalPlayer.Stop();
+
+                backgroundWorker.RunWorkerAsync();
+
+                workStatus = true;
+            }
+            else
+            {
+                Button.Content = "开始";
+
+                finalPlayer.Play();
+
+                backgroundWorker.CancelAsync();
+
+                workStatus = false;
+            }
+        }
+
+        private void Window_Unloaded(object sender, RoutedEventArgs e)
+        {
+            progressPlayer.Close();
+            finalPlayer.Close();
+        }
+
+        // 循环播放
+        private void Media_Ended(object sender, EventArgs e)
+        {
+            progressPlayer.Position = TimeSpan.Zero;
+            progressPlayer.Play();
+        }
+    }
+}
---
+++ RaffleHFHeavyIon/RaffleHelper.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace RaffleHFHeavyIon
+{
+    class RaffleHelper : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(PropertyChangedEventArgs e)
+        {
+            PropertyChanged?.Invoke(this, e);
+        }
+
+        // 排数
+        private int _rowNow = 0;
+
+        public int RowNow
+        {
+            set
+            {
+                _rowNow = value;
+                OnPropertyChanged(new PropertyChangedEventArgs("RowNow"));
+            }
+            get { return _rowNow; }
+        }
+
+        // 列数
+        private int _columnNow = 0;
+
+        public int ColumnNow
+        {
+            set
+            {
+                _columnNow = value;
+                OnPropertyChanged(new PropertyChangedEventArgs("ColumnNow"));
+            }
+            get { return _columnNow; }
+        }
+
+        // 所有排的历史记录
+        private ObservableCollection<double> _rowHistoryCollection = new ObservableCollection<double>();
+
+        public ObservableCollection<double> RowHistoryCollection
+        {
+            set
+            {
+                _rowHistoryCollection = value;
+                OnPropertyChanged(new PropertyChangedEventArgs("RowHistoryCollection"));
+            }
+            get { return _rowHistoryCollection; }
+        }
+
+        // 所有列的历史记录
+        private ObservableCollection<double> _columnHistoryCollection = new ObservableCollection<double>();
+
+        public ObservableCollection<double> ColumnHistoryCollection
+        {
+            set
+            {
+                _columnHistoryCollection = value;
+                OnPropertyChanged(new PropertyChangedEventArgs("ColumnHistoryCollection"));
+            }
+            get { return _columnHistoryCollection; }
+        }
+
+        // 排数和列数的上限(可以取到的上限)
+        private int _rowUpBound = 20;
+        private int _columnUpBound = 20;
+
+        // 随机数产生器
+        private Random rdForRow;
+        private Random rdForColumn;
+
+        // 根据当前时间产生随机数，并且赋值给RowNow和ColumnNow,时间精确到毫秒
+        private void generateRandom()
+        {
+            if(rdForRow == null || rdForColumn == null)
+                throw new Exception("随机数未初始化!");
+            try
+            {
+                int rdNumber = rdForRow.Next(_rowUpBound);
+                rdNumber = rdForColumn.Next(_rowUpBound);
+            }
+            catch (Exception e)
+            {
+                throw new Exception("无法产生随机数!");
+            }
+            // 行数
+            while (true)
+            {
+                int rdNumber = rdForColumn.Next(_rowUpBound);
+                if (rdNumber != 0)
+                {
+                    RowNow = rdNumber;
+                    break;
+                }
+            }
+            //列数
+            while (true)
+            {
+                int rdNumber = rdForColumn.Next(_columnUpBound);
+                if (rdNumber != 0)
+                {
+                    ColumnNow = rdNumber;
+                    break;
+                }
+            }
+        }
+
+        // 间隔一定时间不停产生随机数
+        // timeGap毫秒数
+        public void GenerateRandomSequentially(int timeGap = 50)
+        {
+            generateRandom();
+            Thread.Sleep(timeGap);
+        }
+
+        // 记录历史
+        public void AddHistory()
+        {
+            if (ColumnHistoryCollection != null && RowHistoryCollection != null)
+            {
+                RowHistoryCollection.Add(RowNow);
+                ColumnHistoryCollection.Add(ColumnNow);
+            }
+            else
+            {
+                throw new Exception("初始化出现错误，记录历史的集合为空");
+            }
+        }
+
+        // 初始化函数
+        public RaffleHelper(int rowUpbound, int columnUpBound)
+        {
+            rdForRow = new Random(DateTime.Now.Second * 1000 + DateTime.Now.Millisecond);
+            rdForColumn = new Random(DateTime.Now.Year * 1000 + DateTime.Now.Millisecond);
+
+            _rowUpBound = rowUpbound;
+            _columnUpBound = columnUpBound;
+        }
+    }
+}
